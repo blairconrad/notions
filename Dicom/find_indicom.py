1af77538c52ccef0425095d45eca6538647d44aa
Blair Conrad <blair@blairconrad.com>
2019-03-27 11:39:37 -0400

    Use glob mattings in attribute values

--- Dicom/find_indicom.py
+++ Dicom/find_indicom.py
@@ -10,12 +10,21 @@ import sys
 import argparse
 import pydicom
 import logging
+import fnmatch
 
 
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("matcher", type=str, help="What to match on. Of the form AttributeName=Value.")
-    parser.add_argument("path", default=".", help="path", nargs="?", type=str)
+    parser.add_argument(
+        "matcher", type=str, help="What to match on. Of the form AttributeName=Value. 'Value' may include wildcards."
+    )
+    parser.add_argument(
+        "path",
+        default=".",
+        nargs="?",
+        type=str,
+        help="Directory to look for DICOM files. Defaults to the current directory.",
+    )
     parser.add_argument("--verbose", action="count", default=0)
 
     args = parser.parse_args(arguments)
@@ -28,8 +37,8 @@ def main(arguments):
         for filename in filenames:
             filename = os.path.join(dirpath, filename)
             try:
-                with pydicom.dcmread(filename) as dataset:
-                    if dataset.get(attribute) == value:
+                with pydicom.dcmread(filename, specific_tags=[attribute]) as dataset:
+                    if fnmatch.fnmatch(dataset.get(attribute), value):
                         print(os.path.relpath(filename))
             except pydicom.errors.InvalidDicomError:
                 logging.info("Skipping %s, as it appears not to be a DICOM file", filename)
