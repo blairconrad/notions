b4d119d283e9152d01558a9a035227dffa76eda8
Blair Conrad <blair@blairconrad.com>
2019-05-03 11:33:21 -0400

    Change summarize_dicom quite a bit

--- Dicom/summarize_dicom.py
+++ Dicom/summarize_dicom.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import os
 import sys
 import argparse
+import glob
 import pydicom
 import logging
 
@@ -15,46 +16,81 @@ import logging
 def main(arguments):
 
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(
-        "--include-filenames", default=False, help="whether to output an entry for each filename", type=str
-    )
     parser.add_argument("path", default=".", help="path", nargs="?", type=str)
     parser.add_argument("--verbose", action="count", default=0)
+    parser.add_argument(
+        "--with",
+        dest="with_attributes",
+        metavar="WITH",
+        default="",
+        help="Add these (comma-separated) additional attributes",
+    )
+    parser.add_argument("--without", default="", help="Omit these (comma-separated) attributes")
+    parser.add_argument("--exactly", default="", help="Show exactly these (comma-separated) attributes")
 
     args = parser.parse_args(arguments)
 
     logging.basicConfig(format="", level={1: logging.INFO, 2: logging.DEBUG}.get(args.verbose, logging.WARN))
 
-    if args.include_filenames:
-        format = "%-16s %-16s %-80s %s"
-        headers = ("Patient ID", "Accession Number", "Patient Name", "filename")
-    else:
-        format = "%-16s %-16s %-80s"
-        headers = ("Patient ID", "Accession Number", "Patient Name")
+    attributes = ["PatientID", "PatientName", "AccessionNumber"]
+    if args.without:
+        for attribute_to_skip in args.without.split(","):
+            attributes.remove(attribute_to_skip)
+
+    if args.with_attributes:
+        for attribute_to_add in args.with_attributes.split(","):
+            attributes.append(attribute_to_add)
+
+    if args.exactly:
+        attributes = args.exactly.split(",")
+
+    specific_tags = attributes + ["SOPClassUID"]
+    if "filename" in specific_tags:
+        specific_tags.remove("filename")
 
     results = set()
 
-    for (dirpath, dirnames, filenames) in os.walk(args.path):
-        for filename in filenames:
-            path = os.path.join(dirpath, filename)
-            logging.debug("Checking %s", path)
-            with pydicom.dcmread(path, force=True) as dataset:
-                if dataset.get("SOPClassUID") is None:
-                    logging.info("Skipping %s, as it appears not to be a DICOM file", path)
-                    continue
-                patient_id = dataset.PatientID
-                patient_name = dataset.PatientName
-                accession_number = dataset.AccessionNumber
+    def get_files_from_source(source):
+        if os.path.isfile(source):
+            yield source
+        elif os.path.isdir(source):
+            for (dirpath, dirnames, filenames) in os.walk(source):
+                for filename in filenames:
+                    yield os.path.join(dirpath, filename)
+        else:
+            for expanded_source in glob.glob(source):
+                for file in get_files_from_source(expanded_source):
+                    yield file
 
-                if args.include_filenames:
-                    filename = os.path.relpath(os.path.join(dirpath, filename), args.path)
-                    results.add((patient_id, accession_number, str(patient_name), filename))
-                else:
-                    results.add((patient_id, accession_number, str(patient_name)))
+    for path in get_files_from_source(args.path):
+        logging.debug("Checking %s", path)
+        with pydicom.dcmread(path, force=True, specific_tags=specific_tags) as dataset:
+            dataset.filename = path
+            if dataset.get("SOPClassUID") is None:
+                logging.info("Skipping %s, as it appears not to be a DICOM file", path)
+                continue
+            this_result = [str(dataset.get(attribute, "")) for attribute in attributes]
+            results.add(tuple(this_result))
 
-    print(format % headers)
-    for result in sorted(results):
-        print(format % result)
+    results = sorted(results)
+    print_table(attributes, results)
+
+
+def print_table(headers, body_rows):
+    def find_column_widths(headers, rows):
+        widths = [len(header) for header in headers]
+        for result in rows:
+            for i in range(len(result)):
+                widths[i] = max(widths[i], len(result[i]))
+        return widths
+
+    widths = find_column_widths(headers, body_rows)
+    column_indices = range(len(headers))
+    print("  ".join([headers[i] + (widths[i] - len(headers[i])) * " " for i in column_indices]))
+    print("  ".join(["-" * widths[i] for i in column_indices]))
+
+    for result in body_rows:
+        print("  ".join([result[i] + (widths[i] - len(result[i])) * " " for i in column_indices]))
 
 
 if __name__ == "__main__":
